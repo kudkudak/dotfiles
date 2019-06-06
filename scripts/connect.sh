a87639a4b319dfecba9841237ce1501f980f50e4
kudkudak <staszek.jastrzebski@gmail.com>
2019-06-06 12:48:35 -0400

    updates

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -10,3 +10,10 @@ if [ "$#" == 0 ] || [ "$1" == "edi" ]; then
     sudo umount ~/sshfs/edi
     sshfs v1sjastr@stonesoup.inf.ed.ac.uk:/afs/inf.ed.ac.uk/user/v/v1sjastr/ ~/sshfs/edi -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
 fi
+
+if [ "$1" == "geraslab" ]; then
+    sudo umount ~/sshfs/geraslab
+    sshfs jastrs01@dlr1.nyumc.org:/gpfs/data/geraslab/jastrs01 ~/sshfs/geraslab -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+fi
+
+
